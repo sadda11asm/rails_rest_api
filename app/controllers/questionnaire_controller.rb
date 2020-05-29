74a4eb6439235eeb57a513ebee5ce36520ec85bc
Saddam <saddam.asmatullayev@nu.edu.kz>
2020-05-29 17:34:12 +0600

    final version

--- app/controllers/questionnaire_controller.rb
+++ app/controllers/questionnaire_controller.rb
@@ -7,7 +7,6 @@ class QuestionnaireController < ApplicationController
   def get
     command = GetAQuestion.call(@redis, @token, params[:question_id], params[:answer_id])
     if command.success?
-      # @redis = command.result["redis"]
       render json: command.result
     else
       render json: { error: command.errors }, status: 400
